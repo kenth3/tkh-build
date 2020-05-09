d1a07af6d1515df1d454c6686978b141f3b31e99
Kent <kenth3@gmail.com>
2020-05-09 09:41:49 -0600

    further traefik tweaks

--- server-initial-setup.sh
+++ server-initial-setup.sh
@@ -3,18 +3,6 @@
 
 docker network create web
 
-touch /acme.json
+touch /tkh-build/traefik/acme.json
 
-chmod 600 /acme.json 
-
-docker run -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $PWD/traefik.toml:/traefik.toml \
-  -v $PWD/acme.json:/acme.json \
-  -p 80:80 \
-  -p 443:443 \
-  -l traefik.frontend.rule=Host:monitor.thomaskenthurd.com \
-  -l traefik.port=8080 \
-  --network web \
-  --name traefik \
-  traefik:1.7.2-alpine
+chmod 600 /tkh-build/traefik/acme.json 
